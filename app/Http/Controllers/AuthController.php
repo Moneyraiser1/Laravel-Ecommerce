7bde1680aa151a033e05891c37b3a4e49af98a67
Moneyraiser1 <Salamsamiat45@gmail.com>
2026-08-30 12:48:12 +0100

    Add debug statement to login method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -35,6 +35,7 @@ class AuthController extends Controller
 }
    public function login(Request $request)
 {
+        dd('LOGIN METHOD REACHED');
     $credentials = $request->validate([
         'email' => ['required', 'email'],
         'password' => ['required', 'string'],

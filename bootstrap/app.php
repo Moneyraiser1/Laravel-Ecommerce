5e4e6032cea248bd81b97a72779b9d660433caed
Moneyraiser1 <Salamsamiat45@gmail.com>
2026-08-30 12:21:28 +0100

    Refactor trustProxies middleware configuration
    Refactor middleware configuration to trust proxies.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,18 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
 
-
-->withMiddleware(function (Middleware $middleware): void {
-    $middleware->trustProxies(
-        at: '*',
-        headers: Request::HEADER_X_FORWARDED_FOR |
-                 Request::HEADER_X_FORWARDED_HOST |
-                 Request::HEADER_X_FORWARDED_PORT |
-                 Request::HEADER_X_FORWARDED_PROTO |
-                 Request::HEADER_X_FORWARDED_AWS_ELB
-    );
-});
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -22,8 +12,16 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR |
+                     Request::HEADER_X_FORWARDED_HOST |
+                     Request::HEADER_X_FORWARDED_PORT |
+                     Request::HEADER_X_FORWARDED_PROTO |
+                     Request::HEADER_X_FORWARDED_AWS_ELB
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
